5958172a1206680ab592d44f205dd920efef6d54
dunderhugo <forssellhugo@gmail.com>
2024-01-16 16:37:55 +0100

    Play field added, and player input, so you can put tiles on the board

--- GameCenter_cmd/TicTacToe.cs
+++ GameCenter_cmd/TicTacToe.cs
@@ -9,6 +9,15 @@
             Console.WriteLine($"|    {oWins}      {ties}            {xWins}         |");
             Console.WriteLine("|________________________________|");
         }
+        public static void PrintPlayField()
+        {
+            Console.WriteLine("The playfield is a number between 1-9, type the number on the spot you want to place your game tile ");
+            Console.WriteLine($"1|2|3");
+            Console.WriteLine($"-+-+-");
+            Console.WriteLine($"4|5|6");
+            Console.WriteLine($"-+-+-");
+            Console.WriteLine($"7|8|9");
+        }
         static void TicHelp()
         {
             //TODO: Update text on commands
@@ -25,6 +34,9 @@
         }
         public static void StartTicTacToe()
         {
+            int oWins = 0;
+            int xWins = 0;
+            int ties = 0;
             string comand;
             Console.WriteLine("Welcome to Tic-Tac-Toe!");
             Console.WriteLine("Type 'start' to start the game, or 'rules' to list the rules, Good luck! ");
@@ -53,37 +65,55 @@
 
             }
             while (comand != "stop");
-            
-        }
 
+        }
         public static void TicStartGame()
         {
+            string[] playFieldArr = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
+            PrintPlayField();
             //TODO: Game board
-            
             int i = 1;
-            int oWins = 0;
-            int xWins = 0;
-            int ties = 0;
-            Console.WriteLine("The playfield is a number between 1-9, type the number on the spot you want to place your game tile ");
             PrintWinLoss(5, 3, 11);
             do
             {
                 //TODO: update gameboard
                 if (i % 2 == 0)
                 {
-                    Console.Write("Player X: ");
-                    Console.ReadLine();
+                    PlayFieldPlayerUpdate("X", playFieldArr);
                     i++;
                 }
                 else if (i % 2 == 1)
                 {
-                    Console.Write("Player O: ");
-                    Console.ReadLine();
+                    PlayFieldPlayerUpdate("O", playFieldArr);
                     i--;
                 }
-            //TODO: Check if the game is over, won/tie
+                //TODO: Check if the game is over, won/tie
             }
             while (true);
+        }
+
+        public static void PlayFieldPlayerUpdate(string player,string[] playFieldArr)
+        {
+            //FIXME: Input can crash if empty & a string
+            while (true)
+            {
+                Console.Write($"Player {player}: ");
+                int playerPos = int.Parse(Console.ReadLine()) - 1;
+                if ((playerPos <= 8 && playerPos >= 0) && (playFieldArr[playerPos] == " "))
+                {
+                    playFieldArr[playerPos] = player;
+                    break;
+                }
+                else if (playFieldArr[playerPos] != " ") Console.WriteLine("You can't place a game tile on a square that is not empty.");
+                else if (playerPos < 0 || playerPos > 9) Console.WriteLine("Input must be a number between 1-9");
+                else Console.WriteLine("ERROR");
+
+            }
+            Console.WriteLine($"|{playFieldArr[0]}|{playFieldArr[1]}|{playFieldArr[2]}|");
+            Console.WriteLine($"|-+-+-|");
+            Console.WriteLine($"|{playFieldArr[3]}|{playFieldArr[4]}|{playFieldArr[5]}|");
+            Console.WriteLine($"|-+-+-|");
+            Console.WriteLine($"|{playFieldArr[6]}|{playFieldArr[7]}|{playFieldArr[8]}|");
         }
     }
 }
