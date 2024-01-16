5eda732b1ae8f9c89c361107aaef84017294d5a2
dunderhugo <forssellhugo@gmail.com>
2024-01-16 15:35:31 +0100

    Shell for GamCenter & TicTacToe

--- GameCenter_cmd/Program.cs
+++ GameCenter_cmd/Program.cs
@@ -2,9 +2,50 @@
 {
     internal class Program
     {
+        static void PrintHelp()
+        {
+            Console.WriteLine("*****    Commands:   *****");
+            Console.WriteLine("list -   Lists all the games avalible");
+            Console.WriteLine("start -  Starts game");
+            Console.WriteLine("quit -   Quits the program");
+        }
+        static void PrintGames()
+        {
+            Console.WriteLine("These are all the games that is avalible:");
+            Console.WriteLine("Tic-Tac-Toe  - In Progress");   //NYI
+            Console.WriteLine("Hangman      - NYI");       //NYI
+            Console.WriteLine("Yatzee       - NYI");        //NYI
+            Console.WriteLine("Maze game    - NYI");     //NYI
+            //TODO: When done, more games to implement
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Hugo's game center!");
+            Console.WriteLine("Type 'help' for commands, or 'list' to list all games avalible");
+            string comand;
+            string[] cmd;
+
+            while (true)
+            {
+                Console.Write("> ");
+                comand = Console.ReadLine();
+                cmd = comand.Split();
+                //TODO: Refactor
+                //FIXME: Input error handling
+                if (cmd[0] == "quit") break;
+                else if (cmd[0] == "start")
+                {
+                    TicTacToe.StartTicTacToe();
+                    if (cmd[1] == "tic") TicTacToe.StartTicTacToe();
+                    else if (cmd[1] == "hangman") Console.WriteLine("Hangman    NYI");
+                    else if (cmd[1] == "Yatzee") Console.WriteLine("Yatzee      NYI");
+                    else if (cmd[1] == "Maze") Console.WriteLine("Maze game     NYI");
+                    else Console.WriteLine($"{comand} is not a valid comand, type 'help' to get all the avalible commands");
+                }
+                else if (cmd[0] == "list") PrintGames();
+                else if (cmd[0] == "help") PrintHelp();
+                else Console.WriteLine($"{comand} is not a valid comand, type 'help' to get all the avalible commands");
+            }
         }
     }
 }
---
+++ GameCenter_cmd/TicTacToe.cs
@@ -0,0 +1,81 @@
+﻿namespace GameCenter_cmd
+{
+    internal class TicTacToe
+    {
+        public static void PrintWinLoss(int oWins, int xWins, int ties)
+        {
+            Console.WriteLine("________________________________");
+            Console.WriteLine("|Wins O:     Ties:        Wins X:|");
+            Console.WriteLine($"|    {oWins}      {ties}            {xWins}         |");
+            Console.WriteLine("|________________________________|");
+        }
+        static void TicHelp()
+        {
+            Console.WriteLine("Avalible comands:");
+            Console.WriteLine("rules -  NYI");
+            Console.WriteLine("stop - NYI");
+            Console.WriteLine("start - Starts game");
+        }
+        static void TicRules()
+        {
+            Console.WriteLine("RULES:");
+            Console.WriteLine("NYI");
+        }
+        public static void StartTicTacToe()
+        {
+            string comand;
+            Console.WriteLine("Welcome to Tic-Tac-Toe!");
+            Console.WriteLine("Type 'start' to start the game, or 'rules' to list the rules, Good luck! ");
+            do
+            {
+                Console.Write("> ");
+                comand = Console.ReadLine();
+                switch (comand)
+                {
+                    case "stop":
+                        break;
+                    case "start":
+                        TicStartGame();
+                        break;
+                    case "help":
+                        TicHelp();
+                        break;
+                    case "rules":
+                        TicRules();
+                        break;
+                    default:
+                        Console.WriteLine($"'{comand}' is not a valid comand, type 'help' to get comands or 'stop' to exit game!");
+                        break;
+                }
+
+            }
+            while (comand != "stop");
+        }
+
+        public static void TicStartGame()
+        {
+            int i = 1;
+            int oWins = 0;
+            int xWins = 0;
+            int ties = 0;
+            Console.WriteLine("The playfield is a number between 1-9, type the number on the spot you want to place your game tile ");
+            PrintWinLoss(5, 3, 11);
+            do
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("Player X: ");
+                    Console.ReadLine();
+                    i++;
+                }
+                else if (i % 2 == 1)
+                {
+                    Console.Write("Player O: ");
+                    Console.ReadLine();
+                    i--;
+                }
+            }
+            while (true);
+        }
+    }
+}
